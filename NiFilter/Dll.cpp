08769d54949b0e5545e8e2a12664cd14c41125c3
falahati <s_falahati@hotmail.com>
2013-08-23 19:34:33 +0430

    License added

--- NiFilter/Dll.cpp
+++ NiFilter/Dll.cpp
@@ -1,16 +1,27 @@
-
-//-----------------------------------------------------------
-//															|
-// Soroush Falahati, s_falahati@hotmail.com, falahati.net	|
-//															|
-//-----------------------------------------------------------
-
 //////////////////////////////////////////////////////////////////////////
 //  This file contains routines to register / Unregister the 
 //  Directshow filter 'NiVirtualCam'
 //  We do not use the inbuilt BaseClasses routines as we need to register as
 //  a capture source
 //////////////////////////////////////////////////////////////////////////
+
+/*  
+    Copyright (C) 2013  Soroush Falahati - soroush@falahati.net
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see [http://www.gnu.org/licenses/].
+    */
+
 #pragma comment(lib, "kernel32")
 #pragma comment(lib, "user32")
 #pragma comment(lib, "gdi32")
