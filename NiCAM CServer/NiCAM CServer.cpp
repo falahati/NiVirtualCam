d42ccffa5b6443d82d4b9c967e293eebf39f27f1
falahati <s_falahati@hotmail.com>
2013-08-27 13:25:30 +0430

    Project configured.

---
+++ NiCAM CServer/NiCAM CServer.cpp
@@ -0,0 +1,14 @@
+// NiCAM CServer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+// General headers
+#include <stdio.h>
+// OpenNI headers
+#include <OpenNI.h> 
+using namespace openni;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
